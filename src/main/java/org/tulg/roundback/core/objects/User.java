5aef4a6d655b92a093c1413cc91dcf210c9d783c
Jason Williams <uberlinuxguy@gmail.com>
2020-05-02 12:52:18 -0400

    More work on #4.  User and Session object

--- src/main/java/org/tulg/roundback/core/objects/User.java
+++ src/main/java/org/tulg/roundback/core/objects/User.java
@@ -1,5 +1,19 @@
 package org.tulg.roundback.core.objects;
 
-public class User {
+import org.tulg.roundback.core.RoundBackObject;
+import org.tulg.roundback.master.MasterDB;
+
+public class User extends RoundBackObject{
+
+    private String uuid;
+    private String name; 
+    private String uname; 
+    private String emai; 
+    private int isAdmin;
+
+    public User(){
+        this.table = "rb_users";
+        this.db = new MasterDB(this.table);
+    }
     
 }
