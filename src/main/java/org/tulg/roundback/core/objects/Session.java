b5ae70f36f3daa65eb07485d8566adcbdd5cf9cd
Jason Williams <uberlinuxguy@gmail.com>
2020-05-02 12:52:18 -0400

    More work on #4.  User and Session object

--- src/main/java/org/tulg/roundback/core/objects/Session.java
+++ src/main/java/org/tulg/roundback/core/objects/Session.java
@@ -1,5 +1,19 @@
 package org.tulg.roundback.core.objects;
 
-public class Session {
+import org.tulg.roundback.core.RoundBackObject;
+import org.tulg.roundback.master.MasterDB;
+
+public class Session extends RoundBackObject{
+
+
+    private String uuid; // unique id in the db
+    private String hid; // the id of the endpoint in the netendpoints table.
+    private int start; // unix timestamp the session started
+    private String userid; // the id of the user from Users table
+
+    public Session(){
+        this.table = "rb_sessions";
+        this.db = new MasterDB(this.table);
+    }
     
 }
